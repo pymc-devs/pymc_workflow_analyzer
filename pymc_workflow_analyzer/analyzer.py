77af09c4b792930399b6fcb7f0b90a7d0a7bea4a
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2023-10-22 00:42:53 +0530

    improve analyzer

--- pymc_workflow_analyzer/analyzer.py
+++ pymc_workflow_analyzer/analyzer.py
@@ -1,16 +1,31 @@
 import ast
+from typing import List, Dict, Any
 from .parser import StaticParser
 
-def static_analyze(file_path):
+
+def static_analyzer(file_path: str) -> Dict[str, List[Dict[str, Any]]]:
+    """
+    Analyze a Python file statically.
+
+    :param file_path: Path to the file to analyze.
+    :return: A dictionary containing the report.
+    """
     parser = StaticParser()
     try:
-        with open(file_path, "r") as source:
-            tree = ast.parse(source.read())
-    except (FileNotFoundError, PermissionError, SyntaxError):
-        raise  # Re-throwing the exception to be caught in main.py
+        with open(file_path, "r") as source_file:
+            tree = ast.parse(source_file.read())
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} does not exist.")
+        sys.exit(1)
+    except PermissionError:
+        print(f"Error: You do not have the permission to read the file {file_path}.")
+        sys.exit(1)
+    except SyntaxError as e:
+        print(f"Error: The file {file_path} contains syntax errors.\n{str(e)}")
+        sys.exit(1)
     except Exception as e:
         print(f"An unexpected error occurred: {str(e)}")
-        raise
+        sys.exit(1)
 
     parser.visit(tree)
-    return parser.report()
+    return parser.get_report()
